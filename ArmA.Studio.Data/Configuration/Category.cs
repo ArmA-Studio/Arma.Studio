c6a0f19b47afbd3d4933e92be9e243dff5679753
X39 <X39@users.noreply.github.com>
2017-04-03 14:57:23 +0200

    added IList interface

--- ArmA.Studio.Data/Configuration/Category.cs
+++ ArmA.Studio.Data/Configuration/Category.cs
@@ -7,14 +7,26 @@ using System.Threading.Tasks;
 
 namespace ArmA.Studio.Data.Configuration
 {
-    public class Category : ConfigurationBaseClass, IEnumerable<SubCategory>
-    {
-        private readonly IEnumerable<SubCategory> InnerList;
+    public class Category : ConfigurationBaseClass, IEnumerable<SubCategory>, IList<SubCategory>
+    { 
+        private readonly IList<SubCategory> InnerList;
 
         public IEnumerator<SubCategory> GetEnumerator() => InnerList.GetEnumerator();
         IEnumerator IEnumerable.GetEnumerator() => InnerList.GetEnumerator();
 
-        public Category(IEnumerable<SubCategory> items)
+        public int Count => this.InnerList.Count;
+        public bool IsReadOnly => this.InnerList.IsReadOnly;
+        public SubCategory this[int index] { get { return this.InnerList[index]; } set { this.InnerList[index] = value; } }
+        public int IndexOf(SubCategory item) => this.InnerList.IndexOf(item);
+        public void Insert(int index, SubCategory item) => this.InnerList.Insert(index, item);
+        public void RemoveAt(int index) => this.InnerList.RemoveAt(index);
+        public void Add(SubCategory item) => this.InnerList.Add(item);
+        public void Clear() => this.InnerList.Clear();
+        public bool Contains(SubCategory item) => this.InnerList.Contains(item);
+        public void CopyTo(SubCategory[] array, int arrayIndex) => this.InnerList.CopyTo(array, arrayIndex);
+        public bool Remove(SubCategory item) => this.InnerList.Remove(item);
+        public Category(IEnumerable<SubCategory> items) : this(items.ToList()) { }
+        public Category(IList<SubCategory> items)
         {
             this.InnerList = items;
         }
