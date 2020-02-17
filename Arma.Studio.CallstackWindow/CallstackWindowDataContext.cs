9e28fc47eaf2938d9513cc2cc303d647b990489e
marco.silipo <m.silipo@web.de>
2020-02-17 18:38:06 +0100

    Added Callstack window

---
+++ Arma.Studio.CallstackWindow/CallstackWindowDataContext.cs
@@ -0,0 +1,79 @@
+﻿using Arma.Studio.Data;
+using Arma.Studio.Data.Debugging;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+
+namespace Arma.Studio.CallstackWindow
+{
+    public class CallstackWindowDataContext : DockableBase, IDisposable
+    {
+        #region Collection: Callstack (ObservableCollection<SqfVm.VariableReference>)
+        public ObservableCollection<HaltInfo> Callstack
+        {
+            get => this._Callstack;
+            set
+            {
+                this._Callstack = value;
+                this.RaisePropertyChanged();
+            }
+        }
+        private ObservableCollection<HaltInfo> _Callstack;
+        #endregion
+
+
+        private void MainWindow_DebuggerStateChanged(object sender, EventArgs e)
+        {
+            switch ((Application.Current as IApp).MainWindow.Debugger?.State ?? Data.Debugging.EDebugState.NA)
+            {
+                case Data.Debugging.EDebugState.Running:
+                case Data.Debugging.EDebugState.NA:
+                    this.Callstack = new ObservableCollection<HaltInfo>();
+                    break;
+                case Data.Debugging.EDebugState.Halted:
+                    this.Callstack = new ObservableCollection<HaltInfo>((Application.Current as IApp).MainWindow.Debugger.GetHaltInfos());
+                    break;
+            }
+        }
+
+        public CallstackWindowDataContext()
+        {
+            (Application.Current as IApp).MainWindow.DebuggerStateChanged += this.MainWindow_DebuggerStateChanged;
+            switch ((Application.Current as IApp).MainWindow.Debugger?.State ?? Data.Debugging.EDebugState.NA)
+            {
+                case Data.Debugging.EDebugState.Running:
+                case Data.Debugging.EDebugState.NA:
+                    this.Callstack = new ObservableCollection<HaltInfo>();
+                    break;
+                case Data.Debugging.EDebugState.Halted:
+                    this.Callstack = new ObservableCollection<HaltInfo>((Application.Current as IApp).MainWindow.Debugger.GetHaltInfos());
+                    break;
+            }
+        }
+
+        public override string Title { get => Properties.Language.CallstackWindow; set => throw new NotSupportedException(); }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposedValue)
+            {
+                if (disposing)
+                {
+                    (Application.Current as IApp).MainWindow.DebuggerStateChanged -= this.MainWindow_DebuggerStateChanged;
+                }
+                this.disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+        }
+        #endregion
+    }
+}
---
+++ Arma.Studio.CallstackWindow/PluginMain.cs
@@ -0,0 +1,31 @@
+﻿using Arma.Studio.Data;
+using Arma.Studio.Data.Dockable;
+using Arma.Studio.Data.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Arma.Studio.CallstackWindow
+{
+    public class PluginMain : IPlugin, IDockableProvider
+    {
+        #region IPlugin
+        public Version Version => new Version(1, 0, 0, 0);
+        public string Name => Properties.Language.CallstackWindow_Name;
+        public Task<IUpdateInfo> CheckForUpdate(CancellationToken cancellationToken) => Task.Run(() => default(IUpdateInfo));
+        public Task Initialize(string pluginPath, CancellationToken cancellationToken) => Task.CompletedTask;
+        #endregion
+        #region IDockableProvider
+        public IEnumerable<DockableInfo> Dockables => new DockableInfo[] {
+            DockableInfo.Create(Properties.Language.CallstackWindow, ECreationMode.Anchorable, () => new CallstackWindowDataContext())
+        };
+        public void AddDataTemplates(GenericDataTemplateSelector selector)
+        {
+            selector.AddAllDataTemplatesInAssembly(typeof(PluginMain).Assembly, (s) => s.StartsWith("Arma.Studio.CallstackWindow"));
+        }
+        #endregion
+    }
+}
