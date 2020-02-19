0f1b4cbd6fc8b94bf39c299af2821ce97389a3cc
Marco.Silipo <m.silipo@web.de>
2020-02-19 14:57:56 +0100

    Added immediate window project

---
+++ Arma.Studio.ImmediateWindow/ImmediateWindowDataContext.cs
@@ -0,0 +1,22 @@
+﻿using Arma.Studio.Data;
+using Arma.Studio.Data.UI;
+using ICSharpCode.AvalonEdit.Document;
+using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Arma.Studio.ImmediateWindow
+{
+    public class ImmediateWindowDataContext : DockableBase
+    {
+
+        public TextDocument TextDocument { get; }
+        public ImmediateWindowDataContext()
+        {
+            this.TextDocument = new TextDocument();
+        }
+
+        public ICommand CmdClearOutputWindow => new RelayCommand(() => Application.Current.Dispatcher.Invoke(() => this.TextDocument.Text = string.Empty));
+        public override string Title { get => Properties.Language.ImmediateWindow; set { throw new NotSupportedException(); } }
+    }
+}
---
+++ Arma.Studio.ImmediateWindow/PluginMain.cs
@@ -0,0 +1,34 @@
+﻿using Arma.Studio.Data;
+using Arma.Studio.Data.Dockable;
+using Arma.Studio.Data.Log;
+using Arma.Studio.Data.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Arma.Studio.ImmediateWindow
+{
+    public class PluginMain : IPlugin, IDockableProvider
+    {
+        internal static LoggerCollection Loggers { get; private set; }
+        #region IPlugin
+        public Version Version => new Version(1, 0, 0, 0);
+        public string Name => Properties.Language.ImmediateWindow_Name;
+        public string Description => String.Empty;
+        public Task<IUpdateInfo> CheckForUpdate(CancellationToken cancellationToken) => Task.Run(() => default(IUpdateInfo));
+        public Task Initialize(string pluginPath, CancellationToken cancellationToken) => Task.CompletedTask;
+        #endregion
+        #region IDockableProvider
+        public IEnumerable<DockableInfo> Dockables => new DockableInfo[] {
+            DockableInfo.Create(Properties.Language.ImmediateWindow, ECreationMode.Anchorable, () => new ImmediateWindowDataContext())
+        };
+        public void AddDataTemplates(GenericDataTemplateSelector selector)
+        {
+            selector.AddAllDataTemplatesInAssembly(typeof(PluginMain).Assembly, (s) => s.StartsWith("Arma.Studio.OutputWindow"));
+        }
+        #endregion
+    }
+}
